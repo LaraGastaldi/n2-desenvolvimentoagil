788afca9acb212a355e7a71a87a0253733d080bc
LaraGastaldi <lara.f.gastaldi@gmail.com>
2023-10-31 17:42:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 def encrypt_this(to_encrypt):
   return " ".join(
       [str(ord(x[0])) + x[-1] + x[2:-1] + x[1] for x in to_encrypt.split(" ")])
-
-
-def digital_root(numero):
-  if numero < 10:
-    return numero
-  return digital_root(sum([int(x) for x in str(numero)]))
